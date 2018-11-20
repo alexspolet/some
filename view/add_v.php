04dc75420c5d03400d8ec778d055c2d816b38f25
alexspolet <lelikpolet@gmail.com>
2018-11-20 09:34:27 +0300

    delete directory ./articles

--- view/add_v.php
+++ view/add_v.php
@@ -1,7 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 20.11.18
- * Time: 9:21
- */
+<form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+    <p>Enter title <br><input type="text" value="<?= $title ?>" name="title"></p>
+    <p>Enter text <br><textarea name="text" cols="50" rows="10"><?= $text ?></textarea></p>
+    <input type="submit" value="save">
+</form>
